ea5715414fd90ceb98b074b251f0b2d20650849f
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-08 13:29:33 +0300

    remove todo and addtodo

---
+++ src/state/todolists-reducer.ts
@@ -0,0 +1,25 @@
+import {TodolistsType} from "../App";
+import {v1} from "uuid";
+
+type ActionType = {
+    type: string
+    [key: string]: any
+}
+
+// меня вызовут и дадут мне стейт (почти всегда объект)
+// и инструкцию (action, тоже объект)
+// согласно прописанному type в этом action (инструкции) я поменяю state
+export const todolistsReducer = (state: Array<TodolistsType>, action: ActionType): Array<TodolistsType> => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST': {
+            return state.filter(el => el.id !== action.id)
+        }
+        case 'ADD-TODOLIST' : {
+            let newTodolistId = v1()
+            let newTodolist: TodolistsType = {id: newTodolistId, title: action.title, filter: "all"}
+            return [...state, newTodolist]
+        }
+        default:
+            throw new Error('I don\'t understand this type')
+    }
+}
