636de603dafeaa3ea97d5f7fa5f255a0b370210f
Evgeniy Klimovch <kliasanik@gmail.com>
2024-02-09 10:45:33 +0300

    use dispatch for all functions

--- src/AppWithReducer.tsx
+++ src/AppWithReducer.tsx
@@ -5,8 +5,14 @@ import {v1} from "uuid";
 import {AddItemForm} from "./AddItemForm";
 import {AppBarHelper} from "./AppBar";
 import {Container, Grid, Paper} from "@mui/material";
-import {todolistsReducer} from "./state/todolists-reducer";
-import {tasksReducer} from "./state/tasks-reducer";
+import {
+    AddTodolistAC,
+    ChangeTodolistFilterAC,
+    ChangeTodolistTitleAC,
+    RemoveTodolistAC,
+    todolistsReducer
+} from "./state/todolists-reducer";
+import {addTaskAC, changeTaskStatusAC, changeTaskTitleAC, removeTaskAC, tasksReducer} from "./state/tasks-reducer";
 
 export type FilterValuesType = "all" | "active" | "completed"
 export type TodolistsType = {
@@ -22,12 +28,12 @@ export function AppWithReducer() {
     let todolistID1 = v1()
     let todolistID2 = v1()
 
-    let [todolists, dispatchToTodolists] = useReducer(todolistsReducer,[
+    let [todolists, dispatchToTodolists] = useReducer(todolistsReducer, [
         {id: todolistID1, title: 'KLIApwnz', filter: 'all'},
         {id: todolistID2, title: 'I try to learn JS', filter: 'all'},
     ])
 
-    let [tasks, dispatchToTasks] = useReducer(tasksReducer,{
+    let [tasks, dispatchToTasks] = useReducer(tasksReducer, {
         [todolistID1]: [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
@@ -42,53 +48,42 @@ export function AppWithReducer() {
 
 
     const changeTaskStatus = (id: string, isDone: boolean, todolistID: string) => {
-   // dispatchToTasks()
+        dispatchToTasks(changeTaskStatusAC(id, isDone, todolistID))
     }
 
     const addTask = (title: string, todolistID: string) => {
-        let task = {id: v1(), title: title, isDone: false}
-        let todolistTasks = tasks[todolistID]
-        tasks[todolistID] = [task, ...todolistTasks]
-        setTasks({...tasks})
+        dispatchToTasks(addTaskAC(title, todolistID))
 
     }
 
     const removeTask = (taskId: string, todolistID: string) => {
-        let todolistTasks = tasks[todolistID]
-        tasks[todolistID] = todolistTasks.filter(el => el.id !== taskId)
-        setTasks({...tasks})
+        dispatchToTasks(removeTaskAC(taskId, todolistID))
     }
 
     const changeFilter = (value: FilterValuesType, todolistId: string) => {
-        let todolist = todolists.find(el => el.id === todolistId)
-        if (todolist) {
-            todolist.filter = value
-            setTodolists([...todolists])
-        }
+        dispatchToTodolists(ChangeTodolistFilterAC(todolistId, value))
     }
     const removeTodolist = (id: string) => {
-        setTodolists(todolists.filter(el => el.id !== id))
-        delete tasks[id]
-        setTasks({...tasks})
+        dispatchToTodolists(RemoveTodolistAC(id))
     }
     const addTodolist = (title: string) => {
-        let newTodolistId = v1()
-        let newTodolist: TodolistsType = {id: newTodolistId, title: title, filter: "all"}
-        setTodolists([newTodolist, ...todolists])
-        setTasks({...tasks, [newTodolistId]: []})
+        let newTodo = AddTodolistAC(title)
+        dispatchToTodolists(newTodo)
+        dispatchToTasks(newTodo)
+
     }
     const updateTask = (todolistId: string, taskId: string, title: string) => {
-        setTasks({...tasks, [todolistId]: tasks[todolistId].map(el => el.id === taskId ? {...el, title: title} : el)})
+        dispatchToTasks(changeTaskTitleAC(taskId, title, todolistId))
     }
     const updateTodolist = (todolistId: string, title: string) => {
-        setTodolists(todolists.map(el => el.id === todolistId ? {...el, title: title} : el))
+        dispatchToTodolists(ChangeTodolistTitleAC(todolistId, title))
     }
 
     return (
         <div className="App">
             <AppBarHelper/>
             <Container fixed>
-                <Grid container style={{padding:'20px'}}>
+                <Grid container style={{padding: '20px'}}>
                     <AddItemForm addItem={addTodolist}/>
                 </Grid>
                 <Grid container spacing={3}>
@@ -102,19 +97,19 @@ export function AppWithReducer() {
                             tasksForTodolist = allTodolistsTasks.filter(el => !el.isDone)
                         }
                         return <Grid item>
-                            <Paper style={{padding:'10px'}}>
-                            <Todolist title={el.title}
-                                      task={tasksForTodolist}
-                                      removeTask={removeTask}
-                                      changeFilter={changeFilter}
-                                      addTask={addTask}
-                                      changeTaskStatus={changeTaskStatus}
-                                      id={el.id}
-                                      filter={el.filter}
-                                      removeTodolist={removeTodolist}
-                                      updateTask={updateTask}
-                                      updateTodolist={updateTodolist}
-                            />
+                            <Paper style={{padding: '10px'}}>
+                                <Todolist title={el.title}
+                                          task={tasksForTodolist}
+                                          removeTask={removeTask}
+                                          changeFilter={changeFilter}
+                                          addTask={addTask}
+                                          changeTaskStatus={changeTaskStatus}
+                                          id={el.id}
+                                          filter={el.filter}
+                                          removeTodolist={removeTodolist}
+                                          updateTask={updateTask}
+                                          updateTodolist={updateTodolist}
+                                />
                             </Paper>
                         </Grid>
                     })}
