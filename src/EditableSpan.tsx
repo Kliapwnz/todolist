52a19121847cc8bb2e17e5f48a6f91c645cba81f
Evgeniy Klimovch <kliasanik@gmail.com>
2024-03-03 14:47:06 +0300

    span fix

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,32 +1,32 @@
 import React, {ChangeEvent, useState} from 'react';
 
 type EditableSpanPropsType = {
-    title: string
-    onClick: (title: string) => void
+   title: string
+   onClick: (title: string) => void
 }
 
 export const EditableSpan = (props: EditableSpanPropsType) => {
-    let [title, setTitle] = useState(props.title)
-    const [edit, setEdit] = useState(false)
-    const editHandler = () => {
-        setEdit(!edit)
-        if (edit) {
-            addTask()
-        }
+   let [title, setTitle] = useState(props.title)
+   const [edit, setEdit] = useState(false)
+   const editHandler = () => {
+      setEdit(!edit)
+      if (edit) {
+         addTask()
+      }
 
 
-    }
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        setTitle(e.currentTarget.value)
-    }
-    const addTask = () => {
-        props.onClick(title)
-    }
+   }
+   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+      setTitle(e.currentTarget.value)
+   }
+   const addTask = () => {
+      props.onClick(title)
+   }
 
-    return (
-        edit
-            ? <input value={title} onChange={onChangeHandler} onBlur={editHandler} autoFocus/>
-            : <span onDoubleClick={editHandler}>{props.title}</span>
-    );
+   return (
+      edit
+         ? <input value={title} onChange={onChangeHandler} onBlur={editHandler} autoFocus/>
+         : <span onDoubleClick={editHandler}>{props.title}</span>
+   );
 };
 
