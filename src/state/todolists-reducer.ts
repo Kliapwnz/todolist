70d613ffa9d73e05db8f1dce524b59798b5d6be6
Evgeniy Klimovch <kliasanik@gmail.com>
2024-03-28 11:50:26 +0300

    store fix

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -26,9 +26,7 @@ export type ChangeTodolistFilterActionType = {
    filter: FilterValuesType
 }
 let initialState: Array<TodolistsType> = []
-// меня вызовут и дадут мне стейт (почти всегда объект)
-// и инструкцию (action, тоже объект)
-// согласно прописанному type в этом action (инструкции) я поменяю state
+
 export const todolistsReducer = (state = initialState, action: ActionsType): Array<TodolistsType> => {
    switch (action.type) {
       case 'REMOVE-TODOLIST': {
